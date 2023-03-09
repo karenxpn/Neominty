2fca23d06fa7a6bb607eea7b4da9f470192cc8b5
KALIMI <karenmirakyan@gmail.com>
2023-03-09 19:05:18 +0400

    first commit

---
+++ Banking/BankingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApp.swift
+//  Banking
+//
+//  Created by Karen Mirakyan on 09.03.23.
+//
+
+import SwiftUI
+
+@main
+struct BankingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
