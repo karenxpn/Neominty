dd311973f906e1d6bbc2d7739f356128c02afb2e
KALIMI <karenmirakyan@gmail.com>
2023-03-10 01:10:52 +0400

    firebase integrated

---
+++ Banking/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Banking
+//
+//  Created by Karen Mirakyan on 10.03.23.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
