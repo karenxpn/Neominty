122b877039654dbf2a29920d422b27e5b66fba72
KALIMI <karenmirakyan@gmail.com>
2023-03-11 00:34:47 +0400

    Enable biometric access view

---
+++ Banking/Views/Authentication/CreatePin.swift
@@ -0,0 +1,20 @@
+//
+//  CreatePin.swift
+//  Banking
+//
+//  Created by Karen Mirakyan on 11.03.23.
+//
+
+import SwiftUI
+
+struct CreatePin: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreatePin_Previews: PreviewProvider {
+    static var previews: some View {
+        CreatePin()
+    }
+}
