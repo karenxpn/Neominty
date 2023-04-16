2c571f0422a8cf77e67d37ee2c33cbb6e476c9ba
Karen Mirakyan <karenmirakyan@gmail.com>
2023-04-16 04:44:54 +0400

    money transfer formatting after end editing util added for the textfield

--- Banking/Views/Home/MoneyTransfer/TransferDetailView.swift
+++ Banking/Views/Home/MoneyTransfer/TransferDetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Introspect
 
 struct TransferDetailView: View {
     @EnvironmentObject var transferVM: TransferViewModel
@@ -66,7 +67,7 @@ struct TransferDetailView: View {
                                     .aspectRatio(contentMode: .fill)
                                     .frame(width: 90, height: 90)
                                     .clipShape(Circle())
-
+                                
                             }
                         }
                         
@@ -115,12 +116,8 @@ struct TransferDetailView: View {
                                     .fill(AppColors.lightGray)
                             }
                         
-                        TextField("10", text: $transferVM.transferAmount)
-                            .keyboardType(.phonePad)
-                            .font(.custom(Roboto.bold.rawValue, size: 24))
-                            .foregroundColor(AppColors.darkBlue)
-                            .padding(.leading, 5)
-                            .frame(height: 31)
+                        AmountTextField(text: $transferVM.transferAmount, fontSize: 24)
+                        
                         
                     }.padding([.horizontal, .bottom], 16)
                 }.background {
@@ -187,6 +184,7 @@ struct TransferDetailView: View {
                 navigateToSuccess.toggle()
             }
     }
+
 }
 
 struct TransferDetailView_Previews: PreviewProvider {
@@ -194,4 +192,47 @@ struct TransferDetailView_Previews: PreviewProvider {
         TransferDetailView()
             .environmentObject(TransferViewModel())
     }
+}
+
+
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    let formatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        formatter.maximum = NSNumber(value: Double.greatestFiniteMagnitude)
+        return formatter
+    }()
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Get the current text and cursor position
+        var text = textField.text ?? ""
+        let cursorPosition = textField.offset(from: textField.beginningOfDocument, to: textField.selectedTextRange?.start ?? textField.endOfDocument)
+        
+        // Insert the new string into the text
+        text = text.replacingCharacters(in: Range(range, in: text)!, with: string)
+        
+        // Remove any commas or periods from the text
+        let value = text.replacingOccurrences(of: ",", with: "").replacingOccurrences(of: ".", with: "")
+        
+        // Convert the text to a Double value
+        if let doubleValue = Double(value) {
+            let number = NSNumber(value: doubleValue / 100.0)
+            text = formatter.string(from: number) ?? ""
+        } else {
+            text = ""
+        }
+        
+        // Set the text and cursor position
+        textField.text = text
+        if let newPosition = textField.position(from: textField.beginningOfDocument, offset: cursorPosition + string.count) {
+            textField.selectedTextRange = textField.textRange(from: newPosition, to: newPosition)
+        }
+        
+        // Return false to prevent the textfield from updating the text
+        return false
+    }
+
 }
