97434db828ad71e1373c5b1ed55fa111d7ad3db6
Karen Mirakyan <karenmirakyan@gmail.com>
2023-05-14 20:56:51 +0400

    welcome text if the name is empty

--- Banking/Views/Home/HomeView.swift
+++ Banking/Views/Home/HomeView.swift
@@ -64,7 +64,7 @@ struct HomeView: View {
                 .toolbar {
                     ToolbarItem(placement: .navigationBarLeading) {
                         VStack(alignment: .leading, spacing: 4) {
-                            TextHelper(text: NSLocalizedString("welcomeBack", comment: ""), color: AppColors.gray, fontName: Roboto.medium.rawValue, fontSize: 12)
+                            TextHelper(text: localName.isEmpty ? NSLocalizedString("welcomeToNeominty", comment: "") : NSLocalizedString("welcomeBack", comment: ""), color: AppColors.gray, fontName: Roboto.medium.rawValue, fontSize: 12)
                             
                             TextHelper(text: localName, color: AppColors.darkBlue, fontName: Roboto.bold.rawValue, fontSize: 24)
                         }
