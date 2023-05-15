fd523d10f6bd629c4254c9ccaf2671a1c327971c
Karen Mirakyan <karenmirakyan@gmail.com>
2023-05-16 03:01:12 +0400

    usage of card styles

--- Banking/Views/Home/UserCard.swift
+++ Banking/Views/Home/UserCard.swift
@@ -13,66 +13,16 @@ struct UserCard: View {
     
     var body: some View {
         
-        VStack(alignment: .leading, spacing: 20) {
-            HStack(spacing: 10) {
-                
-                Image("chip")
-                Image("wifi")
-                
-                Spacer()
-                
-                if selected {
-                    Image("check")
-                }
-            }.padding([.horizontal, .top], 20)
-            
-            VStack(alignment: .leading, spacing: 6) {
-                TextHelper(text: card.cardPan, color: .white, fontName: Roboto.bold.rawValue, fontSize: 14)
-                TextHelper(text: card.expirationDate, color: AppColors.gray, fontName: Roboto.regular.rawValue, fontSize: 12)
-            }.padding(.horizontal, 20)
-                .padding(.top, 13)
-            
-            HStack {
-                TextHelper(text: card.cardHolder, color: .white, fontName: Roboto.bold.rawValue, fontSize: 14)
-                Spacer()
-                Image(card.cardType.icon)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 45, height: 26)
-            }.padding(.vertical, 16)
-                .padding(.horizontal, 20)
-                .background {
-                    if card.cardStyle == .blueGreen {
-                        AppColors.green
-                    } else if card.cardStyle == .greenBlue {
-                        AppColors.darkBlue
-                    }
-                }
-        }.background {
-            if card.cardStyle == .blue || card.cardStyle == .blueGreen {
-                ZStack(alignment: .trailing) {
-                    AppColors.darkBlue
-                    if card.cardStyle == .blue {
-                        CardHexDesign()
-                    } else {
-                        Image("card-sign")
-                            .resizable()
-                    }
-                }
-            } else if card.cardStyle == .green || card.cardStyle == .greenBlue {
-                
-                ZStack(alignment: .trailing) {
-                    AppColors.green
-                    if card.cardStyle == .green {
-                        CardHexDesign()
-                    } else {
-                        Image("card-sign")
-                            .resizable()
-                    }
-                }
-            }
+        switch card.cardStyle {
+        case .standardBlue, .standardGreen, .standardBlueGreen, .standardGreenBlue:
+            StandardStyles(card: card, selected: selected)
+        case .hexBlue, .hexGreen, .hexBlueGreen, .hexGreenBlue:
+            HexagonStyles(card: card, selected: selected)
+        case .signedBlueGreen, .signedGreenBlue:
+            SignedStyle(card: card, selected: selected)
+        default:
+            EmptyView()
         }
-        .cornerRadius(16)
     }
 }
 
