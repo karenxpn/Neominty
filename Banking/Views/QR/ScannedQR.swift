5daf2233dc8509aa892b58042d5fdd75813eafd2
Karen Mirakyan <karenmirakyan@gmail.com>
2023-05-21 16:35:41 +0400

    ScannedQR view separated

---
+++ Banking/Views/QR/ScannedQR.swift
@@ -0,0 +1,112 @@
+//
+//  ScannedQR.swift
+//  Banking
+//
+//  Created by Karen Mirakyan on 21.05.23.
+//
+
+import SwiftUI
+import CodeScanner
+
+struct ScannedQR: View {
+    @EnvironmentObject var viewRouter: ViewRouter
+    @EnvironmentObject var qrVM: QrViewModel
+    
+    let result: String
+    @Binding var amount: String
+    @Binding var presented: Bool
+    @Binding var completed: Bool
+
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    var body: some View {
+        
+        AppColors.darkGray
+            .edgesIgnoringSafeArea(.all)
+        
+        VStack(spacing: 20) {
+            
+            Spacer()
+            
+            ZStack {
+                Image(uiImage: generateQRCode(from: result))
+                    .resizable()
+                    .interpolation(.none)
+                    .scaledToFit()
+                    .frame(width: 157, height: 157)
+                    .padding(20)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(color: AppColors.shadow, radius: 25, x: 2, y: 15)
+                
+                Image("scan-success")
+            }
+            
+            VStack {
+                HStack {
+                    TextHelper(text: NSLocalizedString("enterAmount", comment: ""), color: AppColors.gray,
+                               fontName: Roboto.medium.rawValue, fontSize: 12)
+                    
+                    Spacer()
+                    
+                    TextHelper(text: NSLocalizedString("max $12,652", comment: ""), color: AppColors.gray,
+                               fontName: Roboto.medium.rawValue, fontSize: 12)
+                }.padding(16)
+                
+                HStack {
+                    TextHelper(text: "USD", color: AppColors.gray, fontName: Roboto.medium.rawValue, fontSize: 16)
+                        .padding(.vertical, 4)
+                        .padding(.horizontal, 8)
+                        .background {
+                            RoundedRectangle(cornerRadius: 8)
+                                .fill(AppColors.lightGray)
+                        }
+                    
+                    AmountTextField(text: $amount, fontSize: 24)
+                    
+                }.padding([.horizontal, .bottom], 16)
+            }.background {
+                RoundedRectangle(cornerRadius: 16)
+                    .strokeBorder(AppColors.lightGray, lineWidth: 1)
+                    .background {
+                        RoundedRectangle(cornerRadius: 16)
+                            .fill(Color.white)
+                    }
+            }
+            
+            ButtonHelper(disabled: amount.isEmpty || qrVM.loading,
+                         label: qrVM.loading ? NSLocalizedString("pleaseWait", comment: "") : NSLocalizedString("sendMoney", comment: "")) {
+                qrVM.performPayment(account: result, amount: amount)
+            }.navigationDestination(isPresented: $completed) {
+                TransferSuccess(amount: amount, currency: .usd) {
+                    presented.toggle()
+                    viewRouter.popToScanRoot()
+                }
+            }
+            
+        }.padding(24)
+            .padding(.bottom, UIScreen.main.bounds.height * 0.1)
+        
+        
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+
+        if let outputImage = filter.outputImage?.tinted(using: UIColor(red: 29/255, green: 58/255, blue: 112/255, alpha: 1)) {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+}
+
+struct ScannedQR_Previews: PreviewProvider {
+    static var previews: some View {
+        ScannedQR(result: "", amount: .constant("100"), presented: .constant(false), completed: .constant(true))
+    }
+}
